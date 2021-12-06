dba1982b3a2b72c78cfd68b72b15c79ca7ef7537
cris431 <32299513+cris431@users.noreply.github.com>
2021-12-06 03:34:06 -0500

    Update Toppings.java

--- src/main/Toppings.java
+++ src/main/Toppings.java
@@ -1,5 +1,9 @@
 package com.example.a213project5;
 
+/**
+This class defines the object Toppings and lists all possible toppings
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public enum Toppings {
     SAUSAGE, PEPPERONI, HAM, PINEAPPLE, OLIVES, MUSHROOMS, ARTICHOKES, ONIONS
 }
