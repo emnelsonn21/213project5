24d32f81dbd47d4a6bf99629c24f7b62ae113214
cris431 <32299513+cris431@users.noreply.github.com>
2021-12-06 03:29:43 -0500

    Update PizzaMaker.java

--- src/main/PizzaMaker.java
+++ src/main/PizzaMaker.java
@@ -1,7 +1,17 @@
 package com.example.a213project5;
 
+/**
+This class creates an instance of Pizza
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public class PizzaMaker {
 
+    /**
+	Creates an instance of Pizza based on the flavor
+	@param flavor the chosen flavor of pizza
+	@return the instance of Pizza created
+	@author Emily Nelson
+	*/
     public static Pizza createPizza(String flavor) {
 
         if (flavor.equals("Deluxe")) {
